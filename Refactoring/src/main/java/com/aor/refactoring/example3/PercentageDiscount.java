f7fb6385f0f7e856c888752e4011be85119ece26
RitaPeixoto <rita.peixoto2000@gmail.com>
2020-03-30 17:35:12 +0100

    Finished exercise 3

--- Refactoring/src/main/java/com/aor/refactoring/example3/PercentageDiscount.java
+++ Refactoring/src/main/java/com/aor/refactoring/example3/PercentageDiscount.java
@@ -1,4 +1,15 @@
 package com.aor.refactoring.example3;
 
-public class PercentageDiscount {
+public class PercentageDiscount implements Discount {
+    private double discount;
+    public PercentageDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public double applyDiscount(double price) {
+        if (discount > 0)
+            return  price - price * discount;
+        return price;
+    }
 }
